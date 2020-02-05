0caea6a3c591b1c93143a475d1c87917d3d34615
Teklehimanot <enbish.blt@gmail.com>
2020-02-05 17:12:32 +0300

    Updates

---
+++ precache-manifest.3edee9b6815d8a6e529800a32ed45105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1e20b96fb2e65a5ff1754229f17e11",
+    "url": "/myfirstreact/index.html"
+  },
+  {
+    "revision": "524dad36f65eee0ac9f3",
+    "url": "/myfirstreact/static/css/main.cc3e8bf9.chunk.css"
+  },
+  {
+    "revision": "e6437bb54b964f8213d3",
+    "url": "/myfirstreact/static/js/2.ae32754d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myfirstreact/static/js/2.ae32754d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "524dad36f65eee0ac9f3",
+    "url": "/myfirstreact/static/js/main.bcfdc9f9.chunk.js"
+  },
+  {
+    "revision": "6260106b6faa8455a62f",
+    "url": "/myfirstreact/static/js/runtime-main.97bdfc97.js"
+  }
+]);
